21eeb26737c6ec43eac3029f4da5b908855e060e
lilypandey <panlily2002@gmail.com>
2025-03-05 12:25:49 +0530

    Update script.py: Auto calibrated HSV

--- script.py
+++ script.py
@@ -1,77 +1,108 @@
 import cv2
-import numpy
+import numpy as np
+import time
 
-# a helper function required as OpenCV's createTrackbar() needs a callback function
-def hello(x):
-    print("")
+def auto_calibrate(hsv_frame, tol_h=10, tol_s =50 , tol_v=50):
+    height, width = hsv_frame.shape[:2]
+    w, h = 100, 100
+    x= width//2 -50
+    y = height//2 -50
+    roi = hsv_frame[y:y+h, x:x+w]
 
-#setting up the webcam, this opens default webcam, 0 refers to default camera
-cap = cv2.VideoCapture(0)
-if not cap.isOpened():
-    print("Error: Camera not opened.")
-    exit()
+    mean_hsv = cv2.mean(roi)[:3]
+    mean_hsv = np.array(mean_hsv, dtype=np.uint8)
 
-#creating Trackbars
-cv2.namedWindow("bars")
-cv2.createTrackbar("lower_hue", "bars", 0, 180, hello)
-cv2.createTrackbar("lower_saturation", "bars", 0, 255, hello)
-cv2.createTrackbar("lower_value", "bars", 0, 255, hello)
-cv2.createTrackbar("upper_hue", "bars", 180, 180, hello)
-cv2.createTrackbar("upper_saturation", "bars", 255, 255, hello)
-cv2.createTrackbar("upper_value", "bars", 50, 255, hello)
+    lower_bound = np.array([max(0, mean_hsv[0]-tol_h), max(0, mean_hsv[1]-tol_s), max(0, mean_hsv[2]-tol_v)], dtype=np.uint8)
+    upper_bound = np.array([min(180, mean_hsv[0]+tol_h), min(255, mean_hsv[1]+tol_s), min(255, mean_hsv[2]+tol_v)], dtype=np.uint8)
+    print("Auto calibrated lower HSV: ", lower_bound)
+    print("Auto calibrated upper HSV: ", upper_bound)
+    return lower_bound, upper_bound
 
-#capturing the initial frame
-while(True):
-    cv2.waitKey(1000)
-    ret, init_frame = cap.read()
-#flag is a flag which indicates if frame was captured successfully
-    if(ret):
-        break
+def capture_background(cap, delay=0.05, num_frames=60):
+    frames=[]
+    print("Capturing Background. No one should be in frame")
+    for i in range(num_frames):
+        ret, frame = cap.read()
+        if ret:
+            frames.append(frame.astype(np.float32))
+        time.sleep(delay)
+    
+    if frames:
+        avg_frame = np.mean(frames, axis=0).astype(np.uint8)
+    else:
+        avg_frame = None
 
-#processing each frame
-while(True):
-    ret, frame =  cap.read()
-    #converts frame to hsv
-    inspect = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    return avg_frame
 
-    #get hsv values from trackbars
-    upper_hue = cv2.getTrackbarPos("upper_hue", "bars")
-    upper_saturation = cv2.getTrackbarPos("upper_saturation", "bars")
-    upper_value = cv2.getTrackbarPos("upper_value", "bars")
-    lower_hue = cv2.getTrackbarPos("lower_hue", "bars")
-    lower_saturation = cv2.getTrackbarPos("lower_saturation", "bars")
-    lower_value = cv2.getTrackbarPos("lower_value", "bars")
+def process_frame(frame, background, lower_hsv, upper_hsv, kernel):
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    #create a mask
-    upper_hsv = numpy.array([upper_hue, upper_saturation, upper_value])
-    lower_hsv = numpy.array([lower_hue, lower_saturation, lower_value])
-    mask = cv2.inRange(inspect, lower_hsv, upper_hsv)
-    kernel = numpy.ones((3,3), numpy.uint8)
-
+    mask = cv2.inRange(hsv_frame, lower_hsv, upper_hsv)
+    #mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=2) #removes noise
     mask = cv2.medianBlur(mask, 3)
     mask = cv2.dilate(mask, kernel, 8)
+    #mask  = cv2.GaussianBlur(mask, (5,5), 0)
+
     mask_inv = 255-mask
 
-    #get the non cloak area
-    b, g, r = frame[:,:,0], frame[:,:,1], frame[:,:,2]
-    b = cv2.bitwise_and(mask_inv, b)
-    g = cv2.bitwise_and(mask_inv, g)
-    r = cv2.bitwise_and(mask_inv, r)
-    frame_inv = cv2.merge((b, g, r))
+    b, g, r = frame[:,:,0], frame[:, :, 1], frame[:, :, 2]
+    b = cv2.bitwise_and(b, mask_inv)
+    g = cv2.bitwise_and(g, mask_inv)
+    r = cv2.bitwise_and(r, mask_inv)
+    non_cloak_area = cv2.merge((b, g, r))
 
-    #get the cloak area
-    b, g, r = init_frame[:,:,0], init_frame[:,:,1], init_frame[:,:,2]
-    b = cv2.bitwise_and(mask, b)
-    g = cv2.bitwise_and(mask, g)
-    r = cv2.bitwise_and(mask, r)
-    init_frame_inv = cv2.merge((b,g,r))
+    b, g, r = cv2.split(background)
+    cloak_area = cv2.merge((cv2.bitwise_and(b, mask), cv2.bitwise_and(g, mask), cv2.bitwise_and(r, mask)))
 
-    #merge cloak and non cloak area and display
-    total = cv2.bitwise_or(frame_inv, init_frame_inv)
-    cv2.imshow("Harry's Cloak", total)
-    #end program when user clicks on q
-    if(cv2.waitKey(3)==ord('q')):
-        break
+    output = cv2.bitwise_or(non_cloak_area, cloak_area)
+
+    return output
+
+def main():
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        print("Error: Camera Not Found")
+        exit()
+    
+    kernel = np.ones((3,3), np.uint8)
+
+    init_frame = capture_background(cap, delay = 0.05, num_frames = 60)
+    if init_frame is None:
+        print("Failed to Capture Background")
+        exit()
+
+    print("Background Captured.")
+    print("Please position your cloak in the center of frame for calibration and wait for 5 seconds")
+    time.sleep(5)
+
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Failed to capture frame for calibration")
+        exit()
+
+    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_hsv, upper_hsv = auto_calibrate(hsv_frame, tol_h=10, tol_s =50 , tol_v=50)
+
+    print("Invisibilty effect is active.")
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Error, can't capture frame")
+            break
+        
+        final = process_frame(frame, init_frame, lower_hsv, upper_hsv, kernel)
+        cv2.putText(final, "Invisibility active! Press  'q' to exit.", (10, 30), cv2.FONT_ITALIC, 1, (0,0,0), 2, cv2.LINE_AA)
+        cv2.imshow("Harry's Invisibility Cloak", final)
+
+        if cv2.waitKey(3) == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
+    cap.release()
+
+if __name__ == "__main__":
+    main()
 
 cv2.destroyAllWindows()
 cap.release()
